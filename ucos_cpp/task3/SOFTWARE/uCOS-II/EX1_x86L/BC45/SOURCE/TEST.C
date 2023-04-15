d9cbd7f2d7a2059a8cc0de8b2df2c266281c0181
David <davidhong@DaviddeMBP.home>
2023-04-16 01:26:26 +0800

    RTOS Project

---
+++ ucos_cpp/task3/SOFTWARE/uCOS-II/EX1_x86L/BC45/SOURCE/TEST.C
@@ -0,0 +1,250 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                           (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
+*                                           All Rights Reserved
+*
+*                                               EXAMPLE #1
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+#include "stdio.h"
+//#include "stdlib.h"
+/*
+*********************************************************************************************************
+*                                               CONSTANTS
+*********************************************************************************************************
+*/
+
+#define  TASK_STK_SIZE                 512       /* Size of each task's stacks (# of WORDs)            */
+
+/*
+*********************************************************************************************************
+*                                               VARIABLES
+*********************************************************************************************************
+*/
+
+OS_STK        Task1_Stk[TASK_STK_SIZE]; 
+OS_STK        Task2_Stk[TASK_STK_SIZE];
+OS_STK        Task3_Stk[TASK_STK_SIZE];
+
+INT8U         err;
+OS_EVENT      *ResourceMutex1;
+OS_EVENT      *ResourceMutex2;
+
+/*
+*********************************************************************************************************
+*                                           FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+        void  Task1();                       /* Function prototypes of tasks                  */
+        void  Task2();
+	 	void  Task3();
+static  void  TaskStartCreateTasks(void);
+static  void  MutexCreate(void);
+		Node* ListPrintNode(Node *head);
+		Node* ListPrintResource(Node *head);
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                                MAIN
+*********************************************************************************************************
+*/
+
+void  main (void)
+{
+
+	head = NULL;
+	head2 = NULL;
+
+    printf("[Time]  [Event]   [From]  [To]\r\n");
+    OSInit();                                              /* Initialize uC/OS-II                      */
+
+    PC_DOSSaveReturn();                                    /* Save environment to return to DOS        */
+    PC_VectSet(uCOS, OSCtxSw);                             /* Install uC/OS-II's context switch vector */
+
+    TaskStartCreateTasks();
+
+    OS_ENTER_CRITICAL();
+    PC_VectSet(0x08, OSTickISR);                           /* Install uC/OS-II's clock tick ISR        */
+    PC_SetTickRate(OS_TICKS_PER_SEC);                      /* Reprogram tick rate                      */
+	MutexCreate();
+
+    OS_EXIT_CRITICAL();
+    OSTimeSet(0);
+    OSStart();                                             /* Start multitasking                       */
+}
+
+
+/*
+*********************************************************************************************************
+*                                              STARTUP TASK
+*********************************************************************************************************
+*/
+/*********************************************************************************************************
+*                                             CREATE TASKS
+*********************************************************************************************************
+*/
+
+static  void  TaskStartCreateTasks (void)
+{
+        OSTaskCreate(Task1, (void *)0, &Task1_Stk[TASK_STK_SIZE], 3);
+        OSTaskCreate(Task2, (void *)0, &Task2_Stk[TASK_STK_SIZE], 4);
+	 	OSTaskCreate(Task3, (void *)0, &Task3_Stk[TASK_STK_SIZE], 5);
+}
+
+static void MutexCreate(void)
+{
+       ResourceMutex1 = OSMutexCreate(1,&err);
+       ResourceMutex2 = OSMutexCreate(2,&err);
+}
+
+/*
+*********************************************************************************************************
+*                                                  TASKS
+*********************************************************************************************************
+*/
+
+
+void  Task1()
+{
+  int ready;
+  int C = 6;
+  int resource = 0;
+  int status = 0;
+  OSTCBCur->compTime = C;
+  for(;;){
+      ready = OSTime;
+      if(ready < 8){
+            ready = 8 - ready;
+            OSTimeDly(ready);
+      }
+      while(OSTCBCur->compTime > 0){
+        if(status != OSTCBCur->compTime){
+             if(resource == 2){
+                OSMutexPend(ResourceMutex1,0,&err);
+             }
+             else if(resource == 4){
+                OSMutexPend(ResourceMutex2,0,&err);
+             }
+             resource = resource + 1;
+             status = OSTCBCur->compTime;
+         }
+      }
+      OSMutexPost(ResourceMutex2);
+	  OSMutexPost(ResourceMutex1);
+      OSTaskSuspend(3);
+    }
+}
+
+void  Task2()
+{
+  int ready;
+  int C = 7;
+  int resource = 0;
+  int status = 0;
+  OSTCBCur->compTime = C;
+  for(;;){
+      ready = OSTime;
+      if(ready < 4){
+            ready = 4 - ready;
+            OSTimeDly(ready);
+      }
+      while(OSTCBCur->compTime > 0){
+        if(status != OSTCBCur->compTime){
+             if(resource == 2){
+                OSMutexPend(ResourceMutex2,0,&err);
+             }
+             resource = resource + 1;
+             status = OSTCBCur->compTime;
+         }
+      }
+      OSMutexPost(ResourceMutex2);
+	  OSTimeDly(1);
+	  head2 = ListPrintResource(head2);
+	  head = ListPrintNode(head);
+      OSTaskSuspend(4);
+    }
+}
+
+
+void  Task3()
+{
+  int C = 9;
+  int resource = 0;
+  int status = 0;
+  OSTCBCur->compTime = C;
+
+  for(;;){
+      while(OSTCBCur->compTime > 0){
+         if(status != OSTCBCur->compTime){
+            if(resource == 2){
+               OSMutexPend(ResourceMutex1,0,&err);
+            }
+            resource = resource + 1;
+            status = OSTCBCur->compTime;
+         }
+      }
+      OSMutexPost(ResourceMutex1);
+      OSTaskSuspend(5);
+    }
+}
+
+Node* ListPrintResource(Node *head){
+	Node *cur = head;
+	Node *temp;
+	int idx;
+	if(head == NULL){
+		return head;
+	}
+	while(cur != NULL){
+	 	idx = cur->state;
+	 	switch(idx)
+	 	{
+	 	 	case 1:
+                printf("%-10d lock    R%d Prio %d changes to %d\n",cur->ticks, cur->resource, cur->prio_cur, cur->prio_next);
+	 	 	 	break;
+ 	 	 	case 2:
+   	 	 	 	printf("%-10d unlock  R%d Prio %d changes to %d\n",cur->ticks, cur->resource, cur->prio_cur, cur->prio_next);
+	 	 	 	break;
+ 	 	}
+	 	cur = cur->next;
+ 	 	temp = head;
+ 	 	head = head->next;
+	 	free(temp);
+	}
+	return head;
+}
+
+
+Node* ListPrintNode(Node *head){
+	Node *cur = head;
+	Node *temp;
+	int idx;
+	if(head == NULL){
+		return head;
+	}
+	while(cur != NULL){
+	 	idx = cur->state;
+	 	switch(idx)
+	 	{
+	 	 	case 1:
+ 	 	 	 	printf("%-10d%-16s%3d%9d\n", cur->ticks,"Preempt", cur->prio_cur, cur->prio_next);
+	 	 	 	break;
+ 	 	 	case 2:
+          	 	printf("%-10d%-16s%3d%9d\n", cur->ticks,"Complete", cur->prio_cur, cur->prio_next);
+	 	 	 	break;
+ 	 	}
+	 	cur = cur->next;
+ 	 	temp = head;
+ 	 	head = head->next;
+	 	free(temp);
+	}
+	return head;
+}
+
---
+++ ucos_edf/task2/SOFTWARE/uCOS-II/EX1_x86L/BC45/SOURCE/TEST.C
@@ -0,0 +1,163 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                           (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
+*                                           All Rights Reserved
+*
+*                                               EXAMPLE #1
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+#include "stdio.h"
+//#include "stdlib.h"
+/*
+*********************************************************************************************************
+*                                               CONSTANTS
+*********************************************************************************************************
+*/
+
+#define  TASK_STK_SIZE                 512       /* Size of each task's stacks (# of WORDs)            */
+
+/*
+*********************************************************************************************************
+*                                               VARIABLES
+*********************************************************************************************************
+*/
+
+OS_STK        Task1_Stk[TASK_STK_SIZE]; 
+OS_STK        Task2_Stk[TASK_STK_SIZE];
+/*
+*********************************************************************************************************
+*                                           FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+        void  Task1();                       /* Function prototypes of tasks                  */
+        void  Task2();
+static  void  TaskStartCreateTasks(void);
+
+	 	Node* ListPrintNode(Node *head);
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                                MAIN
+*********************************************************************************************************
+*/
+
+void  main (void)
+{
+
+	head = NULL;
+    task_log_index = 0;
+    printf("[Time]  [Event]   [From]  [To]\r\n");
+    OSInit();                                              /* Initialize uC/OS-II                      */
+
+    PC_DOSSaveReturn();                                    /* Save environment to return to DOS        */
+    PC_VectSet(uCOS, OSCtxSw);                             /* Install uC/OS-II's context switch vector */
+
+    TaskStartCreateTasks();
+
+    OS_ENTER_CRITICAL();
+    PC_VectSet(0x08, OSTickISR);                           /* Install uC/OS-II's clock tick ISR        */
+    PC_SetTickRate(OS_TICKS_PER_SEC);                      /* Reprogram tick rate                      */
+    OS_EXIT_CRITICAL();
+    OSTimeSet(0);
+    OSStart();                                             /* Start multitasking                       */
+}
+
+
+/*
+*********************************************************************************************************
+*                                              STARTUP TASK
+*********************************************************************************************************
+*/
+/*********************************************************************************************************
+*                                             CREATE TASKS
+*********************************************************************************************************
+*/
+
+static  void  TaskStartCreateTasks (void)
+{
+        OSTaskCreate(Task1, (void *)0, &Task1_Stk[TASK_STK_SIZE], 1, 1, 3); // Init comptime & deadline in TCB
+        OSTaskCreate(Task2, (void *)0, &Task2_Stk[TASK_STK_SIZE], 2, 3, 5);// (prio, comptimr, deadline)
+}
+
+/*
+*********************************************************************************************************
+*                                                  TASKS
+*********************************************************************************************************
+*/
+
+void  Task1()
+{
+    int C = 1;
+    int P = 3;
+    int start;      /*The Start time*/
+    int end;
+    int toDelay;    
+    start = 0;
+    for (;;) {
+        while(OSTCBCur->compTime > 0); //using cpu
+	 	OS_ENTER_CRITICAL();		   // prevent update deadline to resched
+        end = OSTimeGet();			
+        toDelay = P - (end - start);
+        start = start + P;			   // update task1 starting time
+        OSTCBCur->compTime = C;		   // refresh comptime
+	 	OSTCBCur->deadline += P;	   // update next deadline
+	 	OS_EXIT_CRITICAL();
+        OSTimeDly(toDelay);
+    }
+}
+
+void  Task2()
+{
+  int C = 3;
+  int P = 5;
+  int start;
+  int end;
+  int toDelay;
+  start = 0;
+  for(;;){
+      head = ListPrintNode(head); 		// Print out scheduling state & free list
+      while(OSTCBCur->compTime > 0);	// using cpu
+	  OS_ENTER_CRITICAL();
+      end = OSTimeGet();			
+      toDelay = P - (end - start);
+      start = start + P;				// update task2 starting time 
+      OSTCBCur->compTime = C;  			// refresh comptime
+	  OSTCBCur->deadline += P; 			// update next deadline
+	  OS_EXIT_CRITICAL();
+      OSTimeDly(toDelay);
+    }
+}
+
+Node* ListPrintNode(Node *head){
+	Node *cur = head;
+	Node *temp;
+	int idx;
+	if(head == NULL){
+		return head;
+	}
+	while(cur != NULL){
+	 	idx = cur->state;
+	 	switch(idx)
+	 	{
+	 	 	case 1:
+ 	 	 	 	printf("%-10d%-16s%3d%9d\n", cur->ticks,"Preempt", cur->prio_cur, cur->prio_next);
+	 	 	 	break;
+ 	 	 	case 2:
+          	 	printf("%-10d%-16s%3d%9d\n", cur->ticks,"Complete", cur->prio_cur, cur->prio_next);
+	 	 	 	break;
+ 	 	}
+	 	cur = cur->next;
+ 	 	temp = head;
+ 	 	head = head->next;
+	 	free(temp);
+	}
+	return head;
+}
+
